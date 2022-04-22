714780d7ce031853d8e1ee078c222c9cd3ef6844
physicshi <physicshi@163.com>
2022-04-22 17:43:14 +0800

    feat: texts router

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.7.7 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/zserge/lorca v0.1.10 // indirect
 )
--- main.go
+++ main.go
@@ -3,19 +3,54 @@ package main
 import (
 	"embed"
 	"io/fs"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"path"
+	"path/filepath"
 	"strings"
 	"syscall"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/zserge/lorca"
 )
 
 //go:embed frontend/dist/*
 var FS embed.FS
+
+func TextsController(c *gin.Context) {
+	var json struct {
+		Raw string
+	}
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	} else {
+		exe, err := os.Executable() // 获取当前执行文件的路径
+		if err != nil {
+			log.Fatal(err)
+		}
+		dir := filepath.Dir(exe) // 获取当前执行文件的目录
+		if err != nil {
+			log.Fatal(err)
+		}
+		filename := uuid.New().String()          // 生成随机文件名
+		uploads := filepath.Join(dir, "uploads") // 拼接 uploads 绝对路径
+		err = os.MkdirAll(uploads, os.ModePerm)  // 创建目录
+		if err != nil {
+			log.Fatal(err)
+		}
+		fullpath := path.Join("uploads", filename+".txt")                            // 拼接文件绝对路径
+		err = ioutil.WriteFile(filepath.Join(dir, fullpath), []byte(json.Raw), 0644) // 写入文件
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath}) // 返回文件路径
+	}
+
+}
 
 func main() {
 	go func() {
@@ -23,6 +58,7 @@ func main() {
 		router := gin.Default()
 		staticFiles, _ := fs.Sub(FS, "frontend/dist")
 		router.StaticFS("/static", http.FS(staticFiles))
+		router.POST("/api/v1/texts", TextsController)
 		router.NoRoute(func(c *gin.Context) {
 			path := c.Request.URL.Path
 			if strings.HasPrefix(path, "/static") {
